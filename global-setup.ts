f909edb93cc940ecfce3f80a30cc7a61477297f4
Matt <henrykus888@gmail.com>
2024-01-03 21:13:31 +0100

    Added configs

---
+++ global-setup.ts
@@ -0,0 +1,18 @@
+import { PlaywrightTestConfig, request, expect } from "@playwright/test";
+
+async function globalSetup(config: PlaywrightTestConfig) {
+  const url = "https://restful-booker.herokuapp.com/auth";
+
+  const requestContext = await request.newContext();
+  const response = await requestContext.post(`${url}`, {
+    data: {
+      username: `${process.env.USERNAME_ADMIN}`,
+      password: `${process.env.USER_ADMIN_PASSWORD}`,
+    },
+  });
+
+  const body = await response.json();
+  process.env.TOKEN = body.token;
+}
+
+export default globalSetup;
